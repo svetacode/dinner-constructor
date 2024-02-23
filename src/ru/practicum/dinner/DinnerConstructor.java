1200b7331d215397a7f27f4d2bdb24e859dc3ed1
Svetlana Melnichuk <melnichu-svetlana@yandex.ru>
2024-02-23 19:34:10 +0300

    Реализован генеарция комбо обедов

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -10,20 +10,20 @@ public class DinnerConstructor {
         menu = new HashMap<>();
     }
 
-    public boolean isDishTypeExist(String dishType){
+    public boolean isDishTypeExist(String dishType) {
         return menu.containsKey(dishType);
     }
 
     public boolean addNewDish(String dishType, String dishName) {
         List<String> dishes;
-        if (menu.containsKey(dishType)){
+        if (menu.containsKey(dishType)) {
             dishes = menu.get(dishType);
         } else {
             dishes = new ArrayList<>();
             menu.put(dishType, dishes);
         }
 
-        if (dishes.contains(dishName)){
+        if (dishes.contains(dishName)) {
             return false;
         } else {
             dishes.add(dishName);
@@ -39,10 +39,7 @@ public class DinnerConstructor {
             List<String> comboContent = new ArrayList<>();
             for (String dishType : comboDishTypes) {
                 List<String> availableDishes = menu.get(dishType);
-                int randomDishPosition = 0;
-                if (availableDishes.size()>1){
-                    randomDishPosition = random.nextInt(0, availableDishes.size() - 1);
-                }
+                int randomDishPosition = random.nextInt(0, availableDishes.size());
                 comboContent.add(availableDishes.get(randomDishPosition));
             }
             combos.add(comboContent);
